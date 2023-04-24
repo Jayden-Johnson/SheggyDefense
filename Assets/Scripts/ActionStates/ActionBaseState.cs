ef8ec1e279754576d5108413b283302ea4ab828d
Jayden-Johnson <126756696+Jayden-Johnson@users.noreply.github.com>
2023-04-23 23:58:49 -0500

    Reload/Recoil
    Lots of stuff

---
+++ Assets/Scripts/ActionStates/ActionBaseState.cs
@@ -0,0 +1,9 @@
+using System;
+
+public abstract class ActionBaseState
+{
+    public abstract void EnterState(ActionStateManager actions);
+
+    public abstract void UpdateState(ActionStateManager actions);
+
+}
---
+++ Assets/Scripts/Shooting/WeaponManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    //fire Rate
+    [SerializeField]float fireRate;
+    float fireRateTimer;
+    [SerializeField] bool semiAuto;
+
+    //bullet properties
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform barrelPos;
+    [SerializeField] float bulletVelocity;
+    [SerializeField] int bulletsPerShot;
+    AimStateManager aim;
+    
+    //audio
+    [SerializeField] AudioClip gunShot;
+    AudioSource audioSource;
+
+    ActionStateManager actions;
+    WeaponRecoil recoil;
+    WeaponAmmo ammo;
+
+    Light muzzleFlashLight;
+    ParticleSystem muzzleFlashParticles;
+    float lightIntensity;
+    [SerializeField] float lightReturnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        recoil = GetComponent<WeaponRecoil>();
+        audioSource = GetComponent<AudioSource>();
+        aim = GetComponentInParent<AimStateManager>();
+        ammo = GetComponent<WeaponAmmo>();
+        muzzleFlashLight = GetComponentInChildren<Light>();
+        muzzleFlashParticles = GetComponentInChildren<ParticleSystem>();
+        lightIntensity = muzzleFlashLight.intensity;
+        muzzleFlashLight.intensity = 0;
+        fireRateTimer = fireRate;
+        actions = GetComponentInParent<ActionStateManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ShouldFire()) Fire();
+        muzzleFlashLight.intensity = Mathf.Lerp(muzzleFlashLight.intensity,0,lightReturnSpeed * Time.deltaTime);
+    }
+    bool ShouldFire(){
+        fireRateTimer += Time.deltaTime;
+        if (fireRateTimer < fireRate) return false;
+        if(ammo.currentAmmo==0)return false;
+        if(actions.currentState==actions.Reload) return false;
+        if(semiAuto&&Input.GetKeyDown(KeyCode.Mouse0)) return true;
+        if(!semiAuto&&Input.GetKey(KeyCode.Mouse0)) return true;
+        return false;
+    }
+    void Fire(){
+        fireRateTimer = 0;
+        barrelPos.LookAt(aim.aimPos);
+        audioSource.PlayOneShot(gunShot);
+        recoil.triggerRecoil();
+        TriggerMuzzleFlash();
+        ammo.currentAmmo --;
+        for(int i = 0; i<bulletsPerShot; i ++){
+            GameObject currentBullet = Instantiate(bullet, barrelPos.position,barrelPos.rotation);
+            Rigidbody rb = currentBullet.GetComponent<Rigidbody>();
+            rb.AddForce(barrelPos.forward * bulletVelocity,ForceMode.Impulse);
+        }
+    }
+
+    void TriggerMuzzleFlash(){
+        muzzleFlashParticles.Play();
+        muzzleFlashLight.intensity = lightIntensity;
+    }
+}
